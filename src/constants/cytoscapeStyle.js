3119b38a369444c7ad3a00ca44b1cd0100e11e1f
Dennis Vetter <vetter@em.uni-frankfurt.de>
2021-07-20 13:11:56 +0200

    add style to highlight ethical issue nodes and flag nodes differently add highlight for selected node

--- src/constants/cytoscapeStyle.js
+++ src/constants/cytoscapeStyle.js
@@ -1,51 +1,71 @@
 const cytoscapeStyle = [
     {
-        selector: 'node',
+        selector: "node",
         style: {
-            'content': 'data(label)',
-            'height': '50%',
-            'width': '130%',
-            'text-valign': 'center',
-            'text-halign': 'center',
-            'text-wrap': 'wrap',
-            'text-max-width': '80%',
-            'font-size': '11pt'
+            "content": "data(label)",
+            "height": "50%",
+            "width": "130%",
+            "text-valign": "center",
+            "text-halign": "center",
+            "text-wrap": "wrap",
+            "text-max-width": "80%",
+            "font-size": "11pt"
         }
     },
     {
-        selector: 'edge',
+        selector: "edge",
         style: {
-            'curve-style': 'taxi',
-            'taxi-direction': 'horizontal'
+            "curve-style": "taxi",
+            "taxi-direction": "horizontal"
         }
     },
     {
-        selector: '.principle',
+        selector: ".principle",
         style: {
-            'height': '80%',
-            'width': '210%',
-            'background-color': '#7C96ED',
-            'text-wrap': 'wrap',
-            'text-max-width': '130%',
-            'font-size': '18pt'
+            "height": "80%",
+            "width": "210%",
+            "background-color": "#7C96ED",
+            "text-wrap": "wrap",
+            "text-max-width": "130%",
+            "font-size": "18pt"
         }
     },
     {
-        selector: '.requirement',
+        selector: ".requirement",
         style: {
-            'height': '60%',
-            'width': '160%',
-            'background-color': '#7CEDD3',
-            'text-wrap': 'wrap',
-            'text-max-width': '100%',
-            'font-size': '14pt'
+            "height": "60%",
+            "width": "160%",
+            "background-color": "#7CEDD3",
+            "text-wrap": "wrap",
+            "text-max-width": "100%",
+            "font-size": "14pt"
         }
     },
     {
-        selector: '.sub-requirement',
+        selector: ".sub-requirement",
         style: {
-            'background-color': '#96ED7C'
+            "background-color": "#96ed7c"
         }
+    },
+    {
+        selector: ".ethical-issue",
+        style: {
+            "shape": "round-rectangle",
+            "background-color": "#ff9980"
+        }
+    },
+    {
+        selector: ".flag",
+        style: {
+            "shape": "round-rectangle",
+            "background-color": "#ffea80"
+        }
+    },
+    {
+      selector: ":selected",
+      style: {
+            "background-blacken": 0.3
+      }
     }
 ]
 
