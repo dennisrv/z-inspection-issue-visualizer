7368fd9759d75ceb8ae1c140394fa53727fc4275
Dennis Vetter <vetter@em.uni-frankfurt.de>
2021-08-12 10:46:35 +0200

    change direction of RELATED_TO relationship, store relations in pydantic as list of integers (node ids) instead of complete nodes

--- django-api/src/api/model/ethical_principle.py
+++ django-api/src/api/model/ethical_principle.py
@@ -1,8 +1,8 @@
 from typing import List, Optional
 
 from neomodel import (
-    RelationshipTo,
-    OneOrMore, StructuredNode
+    OneOrMore,
+    RelationshipFrom
 )
 
 from .base_node import (
@@ -10,20 +10,17 @@ from .base_node import (
     BaseNodeOrm
 )
 
-from .key_requirement import KeyRequirementOrm
-
-
 class EthicalPrincipleOrm(BaseNodeOrm):
     # use __label__ if node label should not be the same as the class name
     # taken from https://stackoverflow.com/a/43458696
     __label__ = "EthicalPrinciple"
 
-    related_requirements = RelationshipTo(KeyRequirementOrm, 'RELATED_TO', cardinality=OneOrMore)
+    related_requirements = RelationshipFrom('.key_requirement.KeyRequirementOrm', 'RELATED_TO', cardinality=OneOrMore)
 
     def pydantic_compatible(self):
-        self.related_requirements = self.related_requirements.all()
+        self.related_requirements = [req.id for req in self.related_requirements.all()]
         return self
 
 
 class EthicalPrinciple(BaseNode[EthicalPrincipleOrm]):
-    related_requirements: Optional[List[BaseNode]]
+    related_requirements: Optional[List[int]]
--- django-api/src/api/model/key_requirement.py
+++ django-api/src/api/model/key_requirement.py
@@ -1,12 +1,16 @@
-from typing import List
+from typing import (
+    List,
+    Optional,
+)
 
 from neomodel import (
-    RelationshipFrom, StructuredNode
+    RelationshipTo,
+    OneOrMore,
 )
 
 from .base_node import (
     BaseNode,
-    BaseNodeOrm
+    BaseNodeOrm,
 )
 
 
@@ -14,7 +18,10 @@ class KeyRequirementOrm(BaseNodeOrm):
     # use __label__ if node label should not be the same as the class name
     # taken from https://stackoverflow.com/a/43458696
     __label__ = "KeyRequirement"
+    related_principles = RelationshipTo('.ethical_principle.EthicalPrincipleOrm', 'RELATED_TO', cardinality=OneOrMore)
 
+    def pydantic_compatible(self):
+        self.related_principles = [prin.id for prin in self.related_principles.all()]
 
 class KeyRequirement(BaseNode[KeyRequirementOrm]):
-    pass
+    related_principles: Optional[List[int]]
